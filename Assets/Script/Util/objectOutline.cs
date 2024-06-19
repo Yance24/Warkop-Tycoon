67ff51b44ef328f77ad89323d0092b2275a0a163
Clesuka <hermansyah.mochtar@gmail.com>
2024-06-19 23:23:27 +0700

    Outline Shader Finish

---
+++ Assets/Script/Util/objectOutline.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class HoverOutline : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    public Material outlineMaterial;
+    private Material defaultMaterial;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        defaultMaterial = spriteRenderer.material;
+    }
+
+    void OnMouseEnter()
+    {
+        spriteRenderer.material = outlineMaterial;;
+    }
+
+    void OnMouseExit()
+    {
+        spriteRenderer.material = defaultMaterial;
+    }
+}
