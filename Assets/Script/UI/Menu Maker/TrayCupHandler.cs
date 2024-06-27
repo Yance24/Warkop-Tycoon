6f705ffea60cce330940d0570f4c0dad6a69639f
Yance24 <yachzeet@gmail.com>
2024-06-28 02:25:37 +0700

    Cup Sprite & Icon

--- Assets/Script/UI/Menu Maker/TrayCupHandler.cs
+++ Assets/Script/UI/Menu Maker/TrayCupHandler.cs
@@ -7,37 +7,38 @@ public class TrayCupHandler : MonoBehaviour
     public TextMeshProUGUI menuName;
     public MenuParameter menuParameter;
     
-    public Sprite coffeeSprite;
-    public Sprite teaSprite;
+    // public Sprite coffeeSprite;
+    // public Sprite teaSprite;
     public int index;
 
-    private Sprite defaultSprite;
+    // private Sprite defaultSprite;
     private Image image;
 
     void Awake(){
         image = GetComponent<Image>();
-        defaultSprite = image.sprite;
+        // defaultSprite = image.sprite;
         // Debug.Log(defaultSprite);
     }
 
     public void setCup(MenuParameter menu, int index){
         menuParameter = menu;
-        switch(menu.Type){
-            case MenuParameter.MenuType.tea:
-                image.sprite = teaSprite? teaSprite: defaultSprite;
-                // Debug.Log("tea");
-            break;
+        // switch(menu.Type){
+        //     case MenuParameter.MenuType.tea:
+        //         image.sprite = teaSprite? teaSprite: defaultSprite;
+        //         // Debug.Log("tea");
+        //     break;
 
-            case MenuParameter.MenuType.coffee:
-                image.sprite = coffeeSprite? coffeeSprite: defaultSprite;
-                // Debug.Log("coffee");
-            break;
+        //     case MenuParameter.MenuType.coffee:
+        //         image.sprite = coffeeSprite? coffeeSprite: defaultSprite;
+        //         // Debug.Log("coffee");
+        //     break;
 
-            default:
-                image.sprite = defaultSprite;
-            break;
-        }
+        //     default:
+        //         image.sprite = defaultSprite;
+        //     break;
+        // }
         this.index = index;
         menuName.text = menuParameter.name;
+        image.sprite = menuParameter.Icon;
     }
 }
